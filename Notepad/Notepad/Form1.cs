a3968871a277e737c707231e52f221f8bb0089bc
denisacorina <63784730+denisacorina@users.noreply.github.com>
2022-04-12 01:07:01 +0300

    Add files via upload

---
+++ Notepad/Notepad/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Notepad
+{
+    public partial class Form1 : Form
+    {
+
+        string filePath = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+ 
+
+      
+
+        private void formatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            filePath = "";
+            richTextBox1.Text = "";
+        }       
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog() { Filter = "TextDocument|*.txt", ValidateNames = true, Multiselect = false})
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    using(StreamReader sr = new StreamReader(ofd.FileName))
+                    {
+                        filePath = ofd.FileName;
+                        Task<string> text = sr.ReadToEndAsync();
+                        richTextBox1.Text = text.Result;
+                    }
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        
+            if(filePath == "")
+            {
+                saveAsToolStripMenuItem_Click(sender, e);
+            }
+            else
+            {
+                richTextBox1.SaveFile(filePath, RichTextBoxStreamType.PlainText);
+            }
+        }  
+  
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+            if(filePath == "")
+            {
+                saveFileDialog1.FileName = "Untitled";
+            }
+            if(DialogResult.OK == saveFileDialog1.ShowDialog())
+            {
+                if(Path.GetExtension(saveFileDialog1.FileName) == ".txt")
+                {
+                    richTextBox1.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText);   
+                }
+                else
+                {
+                    richTextBox1.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.RichText);
+
+                }
+                filePath = Path.GetFileName(filePath) + " . Text Editor";
+            }
+        }
+
+        private void printPreviewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.ShowDialog();
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.Document = printDocument1;
+            if(printPreviewDialog1.ShowDialog() == DialogResult.OK)
+            {
+                printDocument1.Print();
+            }
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Redo();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectedText = "";
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+        }
+
+        private void wordWrapToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(wordWrapToolStripMenuItem.Checked)
+            {
+                wordWrapToolStripMenuItem.Checked = false;
+                richTextBox1.WordWrap = false;
+            }
+            else
+            {
+                wordWrapToolStripMenuItem.Checked = true;
+                richTextBox1.WordWrap = true;
+            }
+        }
+
+        //private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString(richTextBox1.Text, richTextBox1.Font, Brushes.Black, 12, 10);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Length > 0)
+            {
+                cutToolStripMenuItem.Enabled = true;
+                copyToolStripMenuItem.Enabled = true;
+            }
+            else
+            {
+                cutToolStripMenuItem.Enabled = false;
+                copyToolStripMenuItem.Enabled = false;
+            }
+        }
+
+        private void zoomInToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            float currentSize;
+            currentSize = richTextBox1.Font.Size;
+            currentSize += 2.0f;
+            richTextBox1.Font = new Font(richTextBox1.Name, currentSize,  
+            richTextBox1.Font.Style, richTextBox1.Font.Unit);   
+        }
+
+        private void zoomOutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            float currentSize;
+            currentSize = richTextBox1.Font.Size;
+            currentSize -= 2.0f;
+            richTextBox1.Font = new Font(richTextBox1.Name, currentSize,
+            richTextBox1.Font.Style, richTextBox1.Font.Unit);
+        }
+
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+
+        //}
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Do you want to save?", "Save", MessageBoxButtons.YesNo);
+            if (dialog == DialogResult.Yes)
+            {
+                if (filePath == "")
+                {
+                    saveFileDialog1.FileName = "Untitled";
+                }
+                if (DialogResult.OK == saveFileDialog1.ShowDialog())
+                {
+                    if (Path.GetExtension(saveFileDialog1.FileName) == ".txt")
+                    {
+                        richTextBox1.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+                    }
+                    else
+                    {
+                        richTextBox1.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.RichText);
+
+                    }
+                    filePath = Path.GetFileName(filePath) + " . Text Editor";
+                }
+            }
+            else if (dialog == DialogResult.No)
+            {
+                Environment.Exit(1);
+              Application.Exit();
+            }
+        }
+
+        
+    }
+}
